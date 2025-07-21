1ce63e23adfc663f5153709fa03da455db8d1178
elbeqqal94 <elbeqqal.youssef@gmail.com>
2025-07-21 21:33:04 +0100

    feature: add list of scnned products.

---
+++ components/ProductListScreen/Product.tsx
@@ -0,0 +1,59 @@
+import { ProductScanResult } from "@/constants/responses";
+import { Colors } from "@/themes/colors";
+import { Typography } from "@/themes/typography";
+import { FC } from "react";
+import { Image, Text, View, StyleSheet } from "react-native";
+
+interface ProductProps {
+  product: ProductScanResult;
+}
+
+const Product: FC<ProductProps> = ({ product }) => {
+  const get_score_color = (score: number) => {
+    if (score < 50) return Colors.RED;
+    else if (score === 50) return Colors.YELLOW;
+    else return Colors.LIGHT_GREEN;
+  };
+  return (
+    <View style={styles.container}>
+      <Image src={product.image_url} style={styles.image} />
+      <View style={styles.text_container}>
+        <Text style={styles.product_name}>{product.product_name}</Text>
+        <Text style={[styles.score, { color: get_score_color(product.score) }]}>
+          {product.score}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    height: 120,
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  image: {
+    width: "30%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  text_container: {
+    flex: 1,
+    height: "100%",
+    paddingLeft: 16,
+  },
+  product_name: {
+    fontWeight: "bold",
+    marginBottom: 8,
+    ...Typography.bodyLarge,
+  },
+  score: {
+    color: Colors.DARK_GRAY,
+    fontWeight: "bold",
+    ...Typography.bodyLarge,
+  },
+});
+
+export default Product;
---
+++ components/ProductListScreen/Products.tsx
@@ -0,0 +1,28 @@
+import { ProductScanResult } from "@/constants/responses";
+import { FC } from "react";
+import { ScrollView, View, StyleSheet } from "react-native";
+import Product from "./Product";
+
+interface ProductProps {
+  products: ProductScanResult[];
+}
+
+const Products: FC<ProductProps> = ({ products }) => {
+  console.log(products);
+  return (
+    <ScrollView style={styles.container}>
+      {products.map((product: ProductScanResult, index: number) => (
+        <Product key={index} product={product} />
+      ))}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
+
+export default Products;
