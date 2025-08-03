d247b906af121c8eb79d243a684f0543049ff36e
elbeqqal94 <elbeqqal.youssef@gmail.com>
2025-08-03 20:08:18 +0100

    bug: fix back action

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,20 +3,12 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
-import { Ionicons } from "@expo/vector-icons";
-import { useFonts } from "expo-font";
-import { Href, Stack, useRouter } from "expo-router";
+import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
 import { useColorScheme } from "@/hooks/useColorScheme";
-import { TouchableOpacity } from "react-native";
 import { Screens } from "@/constants/screens";
-import { Colors } from "@/themes/colors";
-import { Typography } from "@/themes/typography";
-import { i18n } from "@/i18n";
-import { LanguageKey } from "@/constants/keys";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -24,7 +16,6 @@ SplashScreen.preventAutoHideAsync();
 export default function Layout() {
   // Hooks
   const colorScheme = useColorScheme();
-  const router = useRouter();
 
   // Add this useEffect to hide splash screen
   useEffect(() => {
@@ -35,30 +26,6 @@ export default function Layout() {
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen
-          name={Screens.HEALTH_SETUP_SCREEN}
-          options={{
-            title: i18n.t(LanguageKey.HEALTH_PROFILE_SETUP),
-            headerLeft: () => (
-              <TouchableOpacity
-                onPress={() => router.back()}
-                style={{
-                  marginLeft: 10,
-                  padding: 8,
-                }}
-              >
-                <Ionicons name="chevron-back" size={24} color={Colors.WHITE} />
-              </TouchableOpacity>
-            ),
-            headerStyle: {
-              backgroundColor: Colors.GLOVO_YELLOW,
-              ...Typography.h1,
-            },
-            headerTitleStyle: {
-              color: Colors.WHITE,
-            },
-          }}
-        />
         <Stack.Screen
           name={Screens.LOGIN_SCREEN}
           options={{
