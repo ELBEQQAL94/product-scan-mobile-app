c8c53bc0d9cee4d378eb5bacfbb3a55c1759a30a
elbeqqal94 <elbeqqal.youssef@gmail.com>
2025-09-18 16:48:43 +0100

    fix tab issue

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,6 +7,10 @@ import {
 } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 import "react-native-reanimated";
+import {
+  useSafeAreaInsets,
+  SafeAreaView,
+} from "react-native-safe-area-context";
 import { Colors } from "@/themes/colors";
 import CustomScanTabButton from "@/components/ScanScreen/CustomScanTabButton";
 import { LanguageKey } from "@/constants/keys";
@@ -17,105 +21,111 @@ export default function TabLayout() {
   // Hooks
   const { is_arabic } = useLanguage();
   const { t } = useTranslation();
+  const insets = useSafeAreaInsets();
 
   return (
-    <Tabs
-      initialRouteName="index"
-      backBehavior="history"
-      screenOptions={{
-        tabBarActiveTintColor: Colors.GLOVO_GREEN,
-        tabBarInactiveTintColor: Colors.GRAY,
-        tabBarStyle: {
-          height: 80,
-          paddingBottom: 10,
-          paddingTop: 10,
-        },
-        tabBarItemStyle: {
-          paddingVertical: 8,
-          paddingHorizontal: 4,
-        },
-        tabBarLabelStyle: {
-          paddingTop: 4,
-          fontSize: 12,
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="product-list"
-        options={{
+    <SafeAreaView style={{ flex: 1 }} edges={["top"]}>
+      <Tabs
+        initialRouteName="index"
+        backBehavior="history"
+        screenOptions={{
+          tabBarActiveTintColor: Colors.GLOVO_GREEN,
+          tabBarInactiveTintColor: Colors.GRAY,
+          tabBarStyle: {
+            height: 80 + insets.bottom,
+            paddingBottom: insets.bottom + 10,
+            paddingTop: 10,
+          },
+          tabBarItemStyle: {
+            paddingVertical: 8,
+            paddingHorizontal: 4,
+          },
+          tabBarLabelStyle: {
+            paddingTop: 4,
+            fontSize: 12,
+          },
           headerShown: false,
-          title: t(LanguageKey.PRODUCT_LIST),
-          tabBarIcon: ({ focused }) => (
-            <MaterialCommunityIcons
-              name={focused ? "shopping" : "shopping-outline"}
-              size={24}
-              color={Colors.GLOVO_GREEN}
-            />
-          ),
         }}
-      />
-
-      <Tabs.Screen
-        name="halal"
-        options={{
-          headerShown: false,
-          title: t(LanguageKey.HALAL),
-          tabBarIcon: ({ focused }) =>
-            focused ? (
-              <MaterialIcons
-                name="mosque"
-                size={24}
-                color={Colors.GLOVO_GREEN}
-              />
-            ) : (
+      >
+        <Tabs.Screen
+          name="product-list"
+          options={{
+            title: t(LanguageKey.PRODUCT_LIST),
+            tabBarIcon: ({ focused }) => (
               <MaterialCommunityIcons
-                name="mosque"
+                name={focused ? "shopping" : "shopping-outline"}
                 size={24}
                 color={Colors.GLOVO_GREEN}
               />
             ),
-        }}
-      />
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: t(LanguageKey.HOME),
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <CustomScanTabButton focused={focused} />
-          ),
-          tabBarLabel: () => null,
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          headerShown: false,
-          title: t(LanguageKey.PROFILE),
-          tabBarIcon: ({ focused }) =>
-            focused ? (
-              <FontAwesome name="user" size={24} color={Colors.GLOVO_GREEN} />
-            ) : (
-              <FontAwesome5 name="user" size={24} color={Colors.GLOVO_GREEN} />
+          }}
+        />
+
+        <Tabs.Screen
+          name="halal"
+          options={{
+            title: t(LanguageKey.HALAL),
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <MaterialIcons
+                  name="mosque"
+                  size={24}
+                  color={Colors.GLOVO_GREEN}
+                />
+              ) : (
+                <MaterialCommunityIcons
+                  name="mosque"
+                  size={24}
+                  color={Colors.GLOVO_GREEN}
+                />
+              ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: t(LanguageKey.HOME),
+            tabBarIcon: ({ focused }) => (
+              <CustomScanTabButton focused={focused} />
             ),
-          tabBarLabelStyle: {
-            fontSize: is_arabic ? 10 : 12,
-          },
-        }}
-      />
-      <Tabs.Screen
-        name="search"
-        options={{
-          headerShown: false,
-          title: t(LanguageKey.SEARCH),
-          tabBarIcon: ({ focused }) => (
-            <AntDesign name="search1" size={24} color={Colors.GLOVO_GREEN} />
-          ),
-          tabBarLabelStyle: {
-            fontSize: is_arabic ? 10 : 12,
-          },
-        }}
-      />
-    </Tabs>
+            tabBarLabel: () => null,
+          }}
+        />
+
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: t(LanguageKey.PROFILE),
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <FontAwesome name="user" size={24} color={Colors.GLOVO_GREEN} />
+              ) : (
+                <FontAwesome5
+                  name="user"
+                  size={24}
+                  color={Colors.GLOVO_GREEN}
+                />
+              ),
+            tabBarLabelStyle: {
+              fontSize: is_arabic ? 10 : 12,
+            },
+          }}
+        />
+
+        <Tabs.Screen
+          name="search"
+          options={{
+            title: t(LanguageKey.SEARCH),
+            tabBarIcon: ({ focused }) => (
+              <AntDesign name="search1" size={24} color={Colors.GLOVO_GREEN} />
+            ),
+            tabBarLabelStyle: {
+              fontSize: is_arabic ? 10 : 12,
+            },
+          }}
+        />
+      </Tabs>
+    </SafeAreaView>
   );
 }
